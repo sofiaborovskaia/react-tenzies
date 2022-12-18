feb2032a96cb1611410afaaea3e0d2756036e81b
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2022-12-18 22:35:36 +0000

    Render random numbers in each die face

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import Die from "./Die";
+import { useState } from "react";
 
 function App() {
+	function newDice() {
+		let diceArr = [];
+		for (let x = 0; x < 10; x++) {
+			let randomNumber = Math.floor(Math.random() * 6);
+			diceArr.push(randomNumber);
+		}
+
+		return diceArr;
+	}
+
+	const [dice, setDice] = useState(newDice());
+
+	const diceElements = dice.map((die) => {
+		return <Die value={die} key={die} />;
+	});
+
 	return (
 		<main>
-			<div className="dices-wrapper">
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-			</div>
+			<div className="dice-wrapper">{diceElements}</div>
 		</main>
 	);
 }
