03a49b90d059662dd58ee17a06d8c087ab2b1e84
Sofia Borovskaia <sofiaborovskaia@gmail.com>
2022-12-18 23:27:20 +0000

    Add uniquie ids to each die

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Die from "./Die";
 import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
 	// Create array of random numbers
@@ -7,8 +8,10 @@ function App() {
 		let diceArr = [];
 		for (let x = 0; x < 10; x++) {
 			let randomNumber = Math.floor(Math.random() * 6);
-			diceArr.push(randomNumber);
+			let diceObj = { value: randomNumber, isHeld: false, id: nanoid() };
+			diceArr.push(diceObj);
 		}
+		console.log(diceArr);
 		return diceArr;
 	}
 
@@ -17,7 +20,7 @@ function App() {
 
 	// Render random numbers inside dice faces
 	const diceElements = dice.map((die) => {
-		return <Die value={die} />;
+		return <Die value={die.value} key={die.id} />;
 	});
 
 	// On dice roll
